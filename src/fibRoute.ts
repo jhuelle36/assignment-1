b8acf682edcdfc105937e2e3ab34e005fe83f371
Jaden Huelle <jhuelle36@gmail.com>
2025-08-27 00:18:21 +0000

    Removed unnecessary type for result and casted fibonacci response to type number

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,8 +5,8 @@ import fibonacci from "./fib";
 export default (req: Request, res: Response) => {
   const { num } = req.params as { num: string };
 
-  const fibN: number = fibonacci(parseInt(num));
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  const fibN: number = Number(fibonacci(parseInt(num)));
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
